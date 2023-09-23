87c86833a7a9f30e27d8095d074523a4110357da
kedarnathdev <71390186+kedarnathdev@users.noreply.github.com>
2023-09-23 19:58:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { resolve } = require('path');
+const requestIp = require('request-ip');
+const app = express();
+const port = process.env.PORT || 3000;
+const ip = require('ip');
+
+app.use(express.static('static'));
+app.use(requestIp.mw());
+
+app.get('/', (req, res) => {
+  res.send(ip.address());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
